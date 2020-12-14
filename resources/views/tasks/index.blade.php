6abb990da19d36cadc2c97d661c45b36fedbfecd
林江閤 <tkmk7q8qq@gmail.com>
2020-12-14 23:09:26 +0800

    於tasks.index子樣板新增顯示任務

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -10,8 +10,8 @@
 
     <!-- 新任務的表單 -->
         <form action="/task" method="POST" class="form-horizontal">
-        {{ csrf_field() }}
-
+        @csrf
+        @method('POST')
         <!-- 任務名稱 -->
             <div class="form-group">
                 <label for="task-name" class="col-sm-3 control-label">任務</label>
@@ -33,4 +33,38 @@
     </div>
 
     <!-- 代辦：目前任務 -->
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                目前任務
+            </div>
+
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+
+                    <!-- 表頭 -->
+                    <thead>
+                    <th>Task</th>
+                    <th>&nbsp;</th>
+                    </thead>
+
+                    <!-- 表身 -->
+                    <tbody>
+                    @foreach ($tasks as $task)
+                        <tr>
+                            <!-- 任務名稱 -->
+                            <td class="table-text">
+                                <div>{{ $task->name }}</div>
+                            </td>
+
+                            <td>
+                                <!-- 代辦：刪除按鈕 -->
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
 @endsection
